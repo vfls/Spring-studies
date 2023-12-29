32dd5a764cc19e90d4cef00bff3b55eb32a861b7
vfls <vitorfelipe.s@hotmail.com>
2023-12-29 10:57:11 -0300

    Repo and project creation

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/test/java/com/course/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
