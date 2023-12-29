3b1abb670dd5b9c683c73c34a788ad52f36eb76c
vfls <vitorfelipe.s@hotmail.com>
2023-12-29 13:58:55 -0300

    Injecting custom application properties and configuring the Spring Boot server

--- 01-spring-boot-overview/Spring Studies/src/main/java/com/course/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/Spring Studies/src/main/java/com/course/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.course.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
     // expose "/" that return "Hello World"
 
     @GetMapping("/")
