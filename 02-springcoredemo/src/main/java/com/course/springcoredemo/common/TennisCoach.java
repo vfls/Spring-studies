3da1c13c181f956873ff3bca5b53ebc2a887224a
vfls <vitorfelipe.s@hotmail.com>
2024-01-02 11:24:43 -0300

    @Primary

--- 02-springcoredemo/src/main/java/com/course/springcoredemo/common/TennisCoach.java
+++ 02-springcoredemo/src/main/java/com/course/springcoredemo/common/TennisCoach.java
@@ -1,5 +1,6 @@
 package com.course.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- 02-springcoredemo/src/main/java/com/course/springcoredemo/rest/DemoController.java
+++ 02-springcoredemo/src/main/java/com/course/springcoredemo/rest/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController (@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController (Coach theCoach){
         myCoach = theCoach;
     }
 
