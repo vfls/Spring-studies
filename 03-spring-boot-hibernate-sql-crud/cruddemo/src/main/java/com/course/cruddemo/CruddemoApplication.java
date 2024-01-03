032f318bdc314d48da23ed504e1276353311c670
vfls <vitorfelipe.s@hotmail.com>
2024-01-03 10:08:37 -0300

    Saving Java object with JPA

--- 03-spring-boot-hibernate-sql-crud/cruddemo/src/main/java/com/course/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-sql-crud/cruddemo/src/main/java/com/course/cruddemo/CruddemoApplication.java
@@ -1,5 +1,6 @@
 package com.course.cruddemo;
 
+import com.course.cruddemo.dao.StudentDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +9,27 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
-		return runner ->{
-			System.out.println("Hello World");
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
+        };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        //create the student object
+        System.out.println("Creating new student object...");
+        Student tempStudent = new Student("Paul", "Doe", "paul@email.com");
+        //save the student object
+        System.out.println("Saving the student ....");
+        studentDAO.save(tempStudent);
+
+        //display id of the saved student
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
+    }
 }
---
+++ 03-spring-boot-hibernate-sql-crud/cruddemo/src/main/java/com/course/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.course.cruddemo.dao;
+
+import com.course.cruddemo.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
