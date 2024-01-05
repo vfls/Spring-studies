3ac0e633b4211ca96076f3d01a2567f8235a217e
vfls <vitorfelipe.s@hotmail.com>
2024-01-05 15:08:36 -0300

    Querying objects with JPA - finding by last name

--- 03-spring-boot-hibernate-sql-crud/cruddemo/src/main/java/com/course/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-sql-crud/cruddemo/src/main/java/com/course/cruddemo/CruddemoApplication.java
@@ -21,9 +21,19 @@ public class CruddemoApplication {
             //createStudent(studentDAO);
             //createMultipleStudents(studentDAO);
             //redStudent(studentDAO);
-            
-            queryForStudents(studentDAO);
+            //queryForStudents(studentDAO);
+            queryForStudentsByLastName(studentDAO);
         };
+    }
+
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+        //get a list of students
+        List<Student> theStudents = studentDAO.findByLastName("Doe");
+
+        //display list of students
+        for (Student tempStudent: theStudents){
+            System.out.println(tempStudent);
+        }
     }
 
     private void queryForStudents(StudentDAO studentDAO) {
