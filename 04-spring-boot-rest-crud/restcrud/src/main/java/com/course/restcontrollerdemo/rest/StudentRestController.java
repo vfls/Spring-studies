10eca7291eb1f29a3b66885f0e8758485b81835a
vfls <vitorfelipe.s@hotmail.com>
2024-01-06 17:42:50 -0300

    REST Global Exception Handling - best practice

--- 04-spring-boot-rest-crud/restcrud/src/main/java/com/course/restcontrollerdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restcrud/src/main/java/com/course/restcontrollerdemo/rest/StudentRestController.java
@@ -44,36 +44,4 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    //Add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException exc){
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    //add another Exception Handler to catch any exception(all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handlerException (Exception exc){
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-    }
-
 }
